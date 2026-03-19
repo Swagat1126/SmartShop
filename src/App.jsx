eed0459c7579eb5e7c5f3660e564eb95781f9ec5
Swagat1126 <swagatkoreti@gmail.com>
2026-03-19 14:49:13 +0530

    Initial commit - SmartShop project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='bold text-5xl text-green-500 '>Hello, Vite!</h1>
+    </>
+  )
+}
+
+export default App
