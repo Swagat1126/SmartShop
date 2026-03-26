7264ab88d4b93bab038df36ac61c6cb2e2c22bb4
SanikaAkhade <sanikaakhade29@gmail.com>
2026-03-26 19:13:45 +0530

    Add files via upload

---
+++ src/Frontend/components/AdminNavbar.jsx
@@ -0,0 +1,10 @@
+const AdminNavbar = () => {
+  return (
+    <div className="navbar">
+      <h3>Admin Panel</h3>
+      <button>Logout</button>
+    </div>
+  );
+};
+
+export default AdminNavbar;
