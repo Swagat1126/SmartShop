62d31a3b1b37b78f417bd506713e6f42590fdd78
aaratighorpade04-maker <aaratighorpade04@gmail.com>
2026-03-29 20:16:08 +0530

    Delete src/main/java/com/example/smartshop/controller/src/main/java/com/example/smartshop/service directory

--- src/main/java/com/example/smartshop/controller/src/main/java/com/example/smartshop/service/src/main/java/com/example/smartshop/entity/src/main/java/com/example/smartshop/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.smartshop.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.example.smartshop.model.Product;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
